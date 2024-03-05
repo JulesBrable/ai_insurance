90c77cb51ec54140cd20670c66ad9f63993679c9
JulesBrable <jules.brable@inrae.fr>
2024-03-05 16:21:39 +0100

    merge with eunice's scripts

--- src/app/page0.py
+++ src/app/page0.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
 
+sns.set_style("whitegrid")
 
 def plot_continuous(df, num_features):
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> KDE Plot for numerical variables </h1>", unsafe_allow_html=True)
     cols = st.columns([4/5, 1/5], gap="medium")
     
     with cols[1]:
@@ -15,13 +20,13 @@ def plot_continuous(df, num_features):
     n = len(num_features)
     ncols = 3
     nrows = n // ncols + (n % ncols > 0)
-
+    
     fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(15, 5 * nrows))
     if plot_type == 'Histogram':
         fig.suptitle('Distribution of Numerical Features (Histograms)')
     else:
         fig.suptitle('Distribution of Numerical Features (Boxplots)')
-        
+
     axs = axs.flatten()
 
     for i, col in enumerate(num_features):
@@ -38,16 +43,97 @@ def plot_continuous(df, num_features):
     with cols[0]:
         st.pyplot(fig)
 
-
+def plot_pie(df: pd.DataFrame, target: str = "Response"):
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> Pie Plot for the target variable </h1>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.pie(df[target].value_counts(), labels=[0, 1], autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')
+    st.pyplot(fig)
+    
 def plot_heatmap(df, num_features):
-    st.write("Correlation Matrix")
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> Correlation between numerical variables </h1>", unsafe_allow_html=True)
     corr_matrix = df[num_features].corr()
     fig, ax = plt.subplots()
     sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm", ax=ax)
     st.pyplot(fig)
 
 
-def plot_numerical_features(df, num_features):
-    plot_continuous(df, num_features)
+def plot_categorical_distributions(df, cat_features):
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> Barplot for categorical variables </h1>", unsafe_allow_html=True)
+
+    num_rows = 2
+    num_cols = [3, 3]
+
+    fig, axes = plt.subplots(num_rows, max(num_cols), figsize=(14, 6 * num_rows))
+    axes = axes.flatten()
+
+    for idx, col in enumerate(cat_features):
+        proportions = df[col].value_counts(normalize=True)
+        ax = proportions.plot(kind='bar', ax=axes[idx], color='skyblue')
+        for p in ax.patches:
+            ax.annotate(f"{p.get_height() * 100:.2f}%", 
+                        (p.get_x() + p.get_width() / 2., p.get_height()), 
+                        ha='center', va='center', xytext=(0, 10), 
+                        textcoords='offset points')
+
+    for i in range(len(cat_features), num_rows * max(num_cols)):
+        fig.delaxes(axes[i])
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def plot_kde_by_response(df):
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> KDE Plots by Response </h1>", unsafe_allow_html=True)
+
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+
+    sns.kdeplot(data=df, x="Age", hue="Response", ax=axes[0], palette="coolwarm", common_norm=False)
+    axes[0].set_xlabel('Age')
+    axes[0].set_ylabel('Density')
+    axes[0].set_title('Distribution of Age by Response')
+
+    sns.kdeplot(data=df, x="Annual_Premium", hue="Response", ax=axes[1], palette="coolwarm", common_norm=False)
+    axes[1].set_xlabel('Annual Premium')
+    axes[1].set_ylabel('Density')
+    axes[1].set_title('Distribution of Annual Premium by Response')
+
+    sns.kdeplot(data=df, x="Vintage", hue="Response", ax=axes[2], palette="coolwarm", common_norm=False)
+    axes[2].set_xlabel('Vintage')
+    axes[2].set_ylabel('Density')
+    axes[2].set_title('Distribution of Vintage by Response')
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def cramers_v(x, y):
+    """Function to calculate Cramer's V"""
+    confusion_matrix = pd.crosstab(x, y)
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2 / n
+    r, k = confusion_matrix.shape
+    phi2_corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))    
+    r_corr = r - ((r-1)**2)/(n-1)
+    k_corr = k - ((k-1)**2)/(n-1)
+    return np.sqrt(phi2_corr / min((k_corr-1), (r_corr-1)))
+
+
+def plot_cramers_v_heatmap(df, cat_variables):
+    """Plotting function for Cramer's V heatmap"""
+    st.markdown("""<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> 
+                Correlation between categorical variables (Cramer's V) </h1>""",
+                unsafe_allow_html=True)
     
-    plot_heatmap(df, num_features)
+    cramers_results = pd.DataFrame(index=cat_variables, columns=cat_variables)
+    for i in cat_variables:
+        for j in cat_variables:
+            if i == j:
+                cramers_results.loc[i, j] = 1.0
+            else:
+                cramers_results.loc[i, j] = cramers_v(df[i], df[j])
+    cramers_results = cramers_results.astype(float)
+    fig, ax = plt.subplots()
+    sns.heatmap(cramers_results, annot=True, fmt=".2f", cmap='coolwarm', cbar_kws={'label': 'Cramer\'s V'}, ax=ax)
+    st.pyplot(fig)
--- 📚_Presentation.py
+++ 📚_Presentation.py
@@ -1,11 +1,18 @@
 """Menu Page of the App"""
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+from src.etl import load_data
+from src.app.page0 import plot_continuous, plot_heatmap, plot_cramers_v_heatmap, plot_kde_by_response, plot_categorical_distributions, plot_pie
 
 
 st.set_page_config(
     page_title="AI for Actuarial Science: Final Project",
     page_icon="🧐",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
         'Report a bug': "https://github.com/JulesBrable/ai_insurance/issues/new",
@@ -22,17 +29,92 @@ st.set_page_config(
     }
 )
 
-st.header("🏠 🏥 Health Insurance Cross Sell Prediction")
+st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:40px;color:DarkSlateBlue;text-align: center;'> 🏠 Health Insurance Cross Sell Prediction 🏥 </h1>", unsafe_allow_html=True)
+st.subheader(" ")
+st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:35px ;text-align: left;'> Problematic </h1>", unsafe_allow_html=True)
 
-st.subheader("Problematic")
+st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:20px ;text-align: justify;'>An Insurance company that has provided Health Insurance to its customers now need to build a model to predict whether the policyholders (customers) from past year will also be interested in Vehicle Insurance provided by the company. Building a model to predict whether a customer would be interested in Vehicle Insurance is extremely helpful for the company because it can then accordingly plan its communication strategy to reach out to those customers and optimise its business model and revenue. So, the aim of this project is to help the company reach its goal.</h1>", unsafe_allow_html=True)
+st.subheader(" ")
 
-st.markdown("bla bla bla")
+st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:35px ;text-align: left;'> Dataset column descriptions </h1>", unsafe_allow_html=True)
+st.subheader(' ')
 
-st.subheader("Descriptive Statistics")
+
+column_data = {
+    "Column Name": [
+        "id", "Gender", "Age", "Driving_License", "Region_Code", 
+        "Previously_Insured", "Vehicle_Age", "Vehicle_Damage", 
+        "Annual_Premium", "Policy_Sales_Channel", "Vintage", "Response"
+    ],
+    "Column Description": [
+        "Unique ID for the customer", 
+        "Gender of the customer", 
+        "Age of the customer", 
+        "0 - Customer does not have DL, 1 - Customer already has DL", 
+        "Unique code for the region of the customer", 
+        "1 - Customer already has Vehicle Insurance, 0 - Customer doesn't have Vehicle Insurance", 
+        "Age of the Vehicle", 
+        "1 - Customer got his/her vehicle damaged in the past. 0 - Customer didn't get his/her vehicle damaged in the past.", 
+        "The amount customer needs to pay as premium in the year", 
+        "Anonymized Code for the channel of outreaching to the customer ie. Different Agents, Over Mail, Over Phone, In Person, etc.", 
+        "Number of Days, Customer has been associated with the company", 
+        "1 - Customer is interested, 0 - Customer is not interested"
+    ]
+}
+
+data_column_description= pd.DataFrame(column_data)
+st.dataframe(data_column_description, hide_index=True)
+
+df = load_data()
+
+if st.checkbox('Show Dataset'):
+    st.subheader('Dataset first five rows')
+    st.dataframe(df.head(), hide_index=True)
+
+st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:35px ;text-align: left;'> Descriptive Statistics </h1>", unsafe_allow_html=True)
+st.subheader(" ")
+
+
+
+num_features = ['Age', 'Annual_Premium', 'Vintage']  
+cat_features = ['Gender', 'Driving_License', 'Region_Code', 'Previously_Insured', 
+            'Vehicle_Damage', 'Policy_Sales_Channel', 'Vehicle_Age']
 
 tabs = st.tabs(["Charts", "Tables"])
 
 with tabs[0]:
-    st.write("Some Charts Here")
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:35px ;text-align: left;'> Univariate Analysis </h1>", unsafe_allow_html=True)
+    st.subheader(" ")
+    
+    plot_continuous(df, num_features)
+    
+    cols=st.columns([2/3, 1/3], gap="medium")
+    with cols[0]:
+        # Modify Categorical variables list
+        cat_features_modified = cat_features.copy()
+        cat_features_modified.remove("Region_Code")
+        cat_features_modified.remove("Policy_Sales_Channel")
+
+        plot_categorical_distributions(df, cat_features_modified)
+    with cols[1]:
+        plot_pie(df)
+
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:35px ;text-align: left;'> Multivariate Analysis </h1>", unsafe_allow_html=True)
+    st.subheader(" ")
+
+    plot_kde_by_response(df)
+    
+    cols = st.columns(2)
+    with cols[0]:
+        plot_heatmap(df, num_features)
+    with cols[1]:
+        plot_cramers_v_heatmap(df, cat_features)
+    
 with tabs[1]:
-    st.write("Some Tables Here")
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> Statistics for numeric variables </h1>", unsafe_allow_html=True)
+    st.table(df[num_features].describe())
+
+    st.markdown("<h1 style='font-family:Lucida Caligraphy;font-size:30px;color:DarkSlateBlue;text-align: center;'> Statistics for categorical variables </h1>", unsafe_allow_html=True)
+    st.table(df[cat_features].astype(str).describe())    
+
+
