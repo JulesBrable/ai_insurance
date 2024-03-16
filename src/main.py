f90f7ce52f14466d829df7d53c5b561cd1e05774
JulesBrable <jules.brable7@gmail.com>
2024-03-16 08:42:58 +0000

    full pipeline to train ml models

---
+++ src/main.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import argparse
+
+from models.preprocessing import create_preprocessor
+from models.training import get_model_configs, train_model_gscv, save_model_pickle
+from etl import get_params
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--methods",
+    choices=["SMOTE", "OVER", ["SMOTE", "OVER", None]],
+    default=["SMOTE", "OVER", None],
+    help="Resampling methods to apply"
+    )
+parser.add_argument(
+    "--model",
+    choices=['Random Forest', 'Logistic Regression'],
+    default='Random Forest',
+    help="Machine Learning model to train"
+)
+args = parser.parse_args()
+methods = args.methods
+model = args.model
+SEED = 42
+
+df = pd.read_csv("https://minio.lab.sspcloud.fr/jbrablx/ai_insurance/raw/train.csv")
+
+df.drop(['id'], axis=1, inplace=True)
+df.drop_duplicates(inplace=True)
+
+X = df.drop('Response', axis=1)
+y = df['Response']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+num_features = ['Age', 'Annual_Premium', 'Vintage']
+cat_features = [
+    'Gender', 'Vehicle_Age', 'Driving_License', 'Region_Code', 'Previously_Insured',
+    'Vehicle_Damage', 'Policy_Sales_Channel'
+]
+
+params_grid = get_params('grid', model)
+params_skf = get_params('skf')
+params_gscv = get_params('gscv')
+
+preprocessor = create_preprocessor(num_features, cat_features)
+model_instance = get_model_configs()[model]['model'](random_state=SEED)
+
+for method in list(methods):
+    grid_search_fitted = train_model_gscv(
+        model=model_instance,
+        X_train=X_train,
+        y_train=y_train,
+        preprocessor=preprocessor,
+        param_grid=params_grid,
+        params_skf=params_skf,
+        params_gscv=params_gscv,
+        resampling_method=method
+    )
+
+    best_model = grid_search_fitted.best_estimator_
+    save_model_pickle(model, method, best_model)
---
+++ src/models/training.py
@@ -0,0 +1,147 @@
+"""Functions that are used during modelling step"""
+import pickle
+import os
+import logging
+from imblearn.over_sampling import SMOTENC, RandomOverSampler
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+
+def get_model_configs():
+    return {
+        "Random Forest": {
+            "model": RandomForestClassifier,
+            "params": [
+                {"name": "n_estimators", "type": "slider", "min": 10, "max": 500, "default": 100},
+                {"name": "max_depth", "type": "slider", "min": 1, "max": 100, "default": 20}
+            ]
+        },
+        "Logistic Regression": {
+            "model": LogisticRegression,
+            "params": [
+                {"name": "C", "type": "slider", "min": 0.01, "max": 10.0, "default": 1.0}
+            ]
+        }
+    }
+
+
+def fit_smote(X_train, y_train, cat_features):
+    print("SMOTE")
+    print("--------")
+    smote = SMOTENC(random_state=42, k_neighbors=15, categorical_features=cat_features)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
+    return X_train, y_train
+
+
+def fit_over(X_train, y_train):
+    print("RandomOverSampler")
+    print("--------")
+    rs = RandomOverSampler(random_state=42)
+    X_train, y_train = rs.fit_resample(X_train, y_train)
+    return X_train, y_train
+
+
+def build_pipeline(preprocessor, model):
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                               ('model', model)])
+    return pipeline
+
+
+def save_model_pickle(model_name, method, fitted_pipeline):
+    if method not in ["SMOTE", "OVER", None]:
+        raise ValueError("Invalid method. Expected 'SMOTE', 'OVER', or None.")
+    if fitted_pipeline is None:
+        raise ValueError("Pipeline object is None.")
+
+    file_name_suffix = (
+        "resampling_smote.pkl" if method == "SMOTE"
+        else "resampling_over.pkl" if method == "OVER"
+        else "resampling_none.pkl"
+    )
+
+    directory = f"../data/outputs/{model_name}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_path = f"{directory}/{file_name_suffix}"
+
+    with open(file_path, 'wb') as file:
+        pickle.dump(fitted_pipeline, file)
+
+    logging.info(f"Model saved successfully to {file_path}")
+
+
+def build_fit_gcv(X_train, y_train, model_pipe, param_grid, params_skf, params_gscv):
+    """
+    Get the best model using a StratifiedKold & GridSearchCV
+    """
+
+    cv = StratifiedKFold(**params_skf)
+
+    grid_search = GridSearchCV(
+        model_pipe,
+        param_grid,
+        cv=cv,
+        **params_gscv
+    )
+
+    grid_search.fit(X_train, y_train)
+    return grid_search
+
+
+def train_model_gscv(
+    model,
+    X_train,
+    y_train,
+    preprocessor,
+    param_grid,
+    params_skf,
+    params_gscv,
+    resampling_method=None
+        ):
+    """
+    Train model with StratifiedKFold and fine tune hyperparams using GridSearchCV
+    """
+    if resampling_method == "SMOTE":
+        cat_features = [
+            'Gender', 'Driving_License', 'Region_Code', 'Previously_Insured',
+            'Vehicle_Damage', 'Policy_Sales_Channel', 'Vehicle_Age'
+            ]
+        X_train, y_train = fit_smote(X_train, y_train, cat_features)
+
+    elif resampling_method == "OVER":
+        X_train, y_train = fit_over(X_train, y_train)
+    else:
+        pass
+
+    pipeline = build_pipeline(preprocessor, model)
+    grid_search_fitted = build_fit_gcv(
+        X_train, y_train, pipeline, param_grid, params_skf, params_gscv
+        )
+    return grid_search_fitted
+
+
+def train_model_oneshot(
+    model,
+    X_train,
+    y_train,
+    preprocessor,
+    resampling_method=None
+        ):
+    if resampling_method == "SMOTE":
+        cat_features = [
+            'Gender', 'Driving_License', 'Region_Code', 'Previously_Insured',
+            'Vehicle_Damage', 'Policy_Sales_Channel', 'Vehicle_Age'
+            ]
+        X_train, y_train = fit_smote(X_train, y_train, cat_features)
+
+    elif resampling_method == "OVER":
+        X_train, y_train = fit_over(X_train, y_train)
+    else:
+        pass
+
+    pipeline = build_pipeline(preprocessor, model)
+    pipeline.fit(X_train, y_train)
+    return pipeline
