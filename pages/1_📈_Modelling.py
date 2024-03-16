a5f2ed62c2c237364bb37e4c7f7c699336cf1286
JulesBrable <jules.brable7@gmail.com>
2024-03-16 23:52:08 +0000

    automatize display metrics

--- pages/1_📈_Modelling.py
+++ pages/1_📈_Modelling.py
@@ -1,32 +1,11 @@
 """Second page of the App"""
 import streamlit as st
-from sklearn.model_selection import train_test_split
 
 from src.app.utils import load_css, make_config
-from src.etl import load_data
-from src.app.page1 import display_numerical_results
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler
-
-
-def plot_kde_after_normalization(df):
-    df_normalized = df.copy()
-    scaler = StandardScaler()
-    columns_to_normalize = ['Age', 'Annual_Premium', 'Vintage']
-    df_normalized[columns_to_normalize] = scaler.fit_transform(df_normalized[columns_to_normalize])
-
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-    for i, x in enumerate(columns_to_normalize):
-        sns.kdeplot(data=df_normalized, x=x, ax=axes[i], palette="coolwarm", common_norm=False)
-        axes[i].set_xlabel(x)
-        axes[i].set_ylabel('Density')
-        axes[i].set_title(f'Distribution of {x} by Response')
-    plt.tight_layout()
-
-    return st.pyplot(fig)
-
+from src.models.preprocessing import prepare_data
+import src.app.page1 as p1
+import src.models.evaluation as eval
+from src.etl import get_params
 
 make_config()
 load_css('static/styles.css')
@@ -36,31 +15,25 @@ st.subheader("Experimentation Results")
 
 tabs = st.tabs(["Machine Learning", "Deep Learning"])
 
-df = load_data()
-X = df.drop('Response', axis=1)
-y = df['Response']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = prepare_data()
 
+models = ["Logistic Regression", "Random Forest"]
+methods = [None, "SMOTE", "OVER"]
+df_combined, classif_reports, best_params = p1.cache_numerical_results(
+    models, methods, X_test, y_test
+    )
+
+logistic_params = get_params('grid', models[0], 'conf/params.yaml')
+random_forest_params = get_params('grid', models[1], 'conf/params.yaml')
 with tabs[0]:
-
     st.markdown("""
                 As we see in the first section, we face a binary classification
                 with class imbalanced. So we tried to find a good model to predict
-                our label that is the variable.
+                our label that is the variable. \n
                 """, unsafe_allow_html=True)
-    st.subheader(" ")
-
-    num_features = ['Age', 'Annual_Premium', 'Vintage']
-    cat_features = [
-        'Gender', 'Driving_License', 'Region_Code', 'Previously_Insured', 'Vehicle_Damage',
-        'Policy_Sales_Channel', 'Vehicle_Age'
-        ]
-
     st.markdown(
-        """<h1 class='h1-preprocessing-steps'> Preprocessing </h1>""", unsafe_allow_html=True
+        """<h1 class='h1-preprocessing-steps'> Preprocessing </h1> \n""", unsafe_allow_html=True
         )
-    st.subheader(" ")
-
     st.markdown("""
                 <div class="h1-details">
                 From the foregoing, we need to process our variables before training a model.
@@ -71,20 +44,17 @@ with tabs[0]:
                     <li>Given that the target variable is imbalanced, it can be managed directly through parameters in the models or by employing <b>resampling methods</b> to balance it</li>
                     <li>Finally, for any chosen model, we will train the model using <b>cross-validation</b> to find the best parameters and thereby reduce overfitting</li>
                 </ul>
-                </div>
+            </div> \n
                 """, unsafe_allow_html=True)
-
-    st.subheader(" ")
 
     if st.checkbox('Show Numerical features distributions after normalization'):
         st.subheader('KDE plot for normalized features')
-        plot_kde_after_normalization(df)
+        p1.plot_kde_after_normalization(X_train)
 
-    st.markdown("""<h1 class='h1-preprocessing-steps'> Model Choice </h1>""",
-            unsafe_allow_html=True)
+    st.markdown(
+        """<h1 class='h1-preprocessing-steps'> Model Choice </h1>""", unsafe_allow_html=True)
 
-    st.markdown("""<h1 class='h1-sub-title'> Metrics </h1>""",
-            unsafe_allow_html=True)
+    st.markdown("""<h1 class='h1-sub-title'> Metrics </h1>""", unsafe_allow_html=True)
 
     st.markdown("""
             <div class="h1-details">
@@ -92,204 +62,68 @@ with tabs[0]:
             <ul>
                 <li><b>Roc Auc Score : </b> Indicates how effectively our model distinguishes between classes.</li>
                 <li><b>Recall : </b> Measures the proportion of actual positives correctly identified by the model.</li>
-            </ul> 
+            </ul>
                 In the context of prospecting, prioritizing a model that identifies all individuals with a high likelihood
-                of subscription is preferable, even at the cost of including some who won't subscribe. 
+                of subscription is preferable, even at the cost of including some who won't subscribe.
                 This approach ensures that no potential subscriber is overlooked.
-            </div>
+            </div> \n
             """, unsafe_allow_html=True)
-    st.subheader(" ")
 
-    st.markdown("""<h1 class='h1-sub-title'> Cross-Validation</h1>""",
-            unsafe_allow_html=True)
-    st.subheader(" ")
+    st.markdown(
+        """<h1 class='h1-sub-title'> Cross-Validation</h1> \n""", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
-
     with col1:
-        st.markdown("""
-        <div style="background-color: #FFCCCC; padding: 10px; border-radius: 5px; font-family: Lucida Caligraphy;">
-            <p style="text-decoration: underline;text-align: center;font-size: 18px;">
-                    <b>Logistic Regression</b>
-            </p>
-            <p> Optimized parameters : </p>
-                <ul>
-                    <li><b>Resampling Methods : </b> None, SmoteNC, RandomOverSampling</li>
-                    <li><b>C : </b> 0.001, 0.01, 0.1, 1, 10, 100 </li>
-                    <li><b>solver : </b> 'newton-cg', 'lbfgs', 'liblinear' </li>
-                    <li><b>max_iter : </b>100, 200, 500 </li>
-                </ul> 
-            <p> Best Model Parameters : </p>  
-                <ul>
-                    <li><b>None : </b> C =  0.1, max_iter = 100, solver ='newton-cg'
-                    Roc AUC score: 0.85 </li>
-                    <li><b>Smote :</b> C = 0.1, max_iter = 100, solver ='lbfgs'}
-                    Roc AUC score: 0.90 </li>
-                    <li><b>OverSampling : </b> C = 10, max_iter = 100, solver = 'liblinear'}
-                    Roc AUC score: 0.85 </li>
-                </ul>   
-        </div>
-        """, unsafe_allow_html=True)
-    with col2:
-        st.markdown("""
-        <div style="background-color: #FFCCCC; padding: 10px; border-radius: 5px; font-family: Lucida Caligraphy;">
-            <p style="text-decoration: underline;text-align: center;font-size: 18px;">
-                    <b>Random Forest</b>
-            </p>
-            <p> Optimized parameters : </p>
-                <ul>
-                    <li><b>Resampling Methods : </b> None, SmoteNC, RandomOverSampling</li>
-                    <li><b>n_estimators : </b> 50, 100, 200</li>
-                    <li><b>max_depth : </b> None, 5, 10, 20 </li>
-                    <li><b>min_samples_split : </b> 2, 5, 10 </li>
-                </ul> 
-            <p> Best Model Parameters : </p> 
-                <ul>
-                    <li><b>None : </b> max_depth =20, min_samples_split= 10, _n_estimators = 200
-                    Roc AUC score: 0.85</li>
-                    <li><b>Smote :</b> max_depth = None, min_samples_split=10, n_estimators =200
-                    Roc AUC score: 0.92</li>
-                    <li><b>OverSampling : </b> max_depth = None, min_samples_split = 2, n_estimators = 200
-                    Roc AUC score : 0.991</li>
-                </ul>  
-        </div>
-        """, unsafe_allow_html=True)
-    st.subheader(" ")
-
-    st.markdown("""<h1 class='h1-sub-title'> Models results </h1>""",
-            unsafe_allow_html=True)
-    st.subheader(" ")
-
-    models = ["Logistic Regression", "Random Forest"]
-    methods = [None, "SMOTE", "OVER"]
-    import pandas as pd
-    dfs = []
-    for mod in models:
-        for method in methods:
-            metrics = display_numerical_results(mod, method, X_test, y_test)
-            full_name = mod + " (resampling = " + str(method) + ")"
-            metrics.set_index('Metric', inplace=True)
-            metrics.columns = [full_name]
-            dfs.append(metrics)
-    df_combined = pd.concat(dfs, axis=1)
-    st.table(df_combined)
-    # for model_name in models:
-    #     for method in methods:
-    #         set_index('Metric', inplace=True)
-    #     mods = st.columns(2)
-    #     with mods[0]:
-    #         st.markdown(f"**Model = {model_name}, resampling = SMOTE**")
-    #         display_numerical_results(model_name, "SMOTE", X_test, y_test)
-    #     with mods[1]:
-    #         st.markdown(f"**Model = {model_name}, resampling = OVER**")
-    #         display_numerical_results(model_name, "OVER", X_test, y_test)
-    #     with st.columns(2)[0]:
-    #         st.markdown(f"**Model = {model_name}, resampling = None**")
-    #         display_numerical_results(model_name, None, X_test, y_test)
-
-    # with mod1 :
-    #     st.dataframe(p1.lr_df, hide_index=True)
-    # with mod2 :
-    #     st.dataframe(p1.lr_smote_df, hide_index=True)
-    # with mod3 :
-    #     st.dataframe(p1.lr_over_df, hide_index=True)
-    # with mod4 :
-    #     st.dataframe(p1.rf_df, hide_index=True)
-    # with mod5 :
-    #     st.dataframe(p1.rf_smote_df, hide_index=True)
-    # with mod6 :
-    #     st.dataframe(p1.rf_over_df, hide_index=True)
-    
-    st.subheader(" ")
-    
-    st.markdown("""<h1 class='h1-sub-title'> If we have to choose ... </h1>""",
-            unsafe_allow_html=True)
-    st.subheader(" ")
-
-    col1, col2 = st.columns(2)
-    # with col1:
-    #     st.image('images/cm_lr_over.png',use_column_width ='always')
-    # with col2:
-    #     st.image('images/roc_lr_over.png',use_column_width ='always')
-
-    from sklearn.metrics import confusion_matrix, roc_auc_score
-    import src.models.evaluation as eval
-
-    def display_confusion_matrix(model_name, method, X_test):
-        model = eval.get_model(model_name, method)
-        y_proba, y_pred = eval.predict_from_fitted_model(model, X_test)
-        cm = confusion_matrix(y_test, y_pred)
-        fig, ax = plt.subplots(figsize=(10, 7))
-        sns.heatmap(
-            cm, annot=True, fmt="d", cmap="Blues",
-            xticklabels=["Predicted 0", "Predicted 1"],
-            yticklabels=["Actual 0", "Actual 1"], ax=ax
+        html_content_logistic = p1.generate_model_params_html(
+            models[0], logistic_params, best_params[models[0]]
             )
-        ax.set_title("Confusion Matrix")
-        ax.set_ylabel("Actual label")
-        ax.set_xlabel("Predicted label")
-        return st.pyplot(fig)
+        st.markdown(html_content_logistic, unsafe_allow_html=True)
 
-    if st.checkbox('Display results for each model'):
-        # options = [
-        #     "Logistic Regression",
-        #     "Logistic Regression with SMOTENC",
-        #     "Logistic Regression with RandomOverSampling",
-        #     "Random Forest",
-        #     "Random Forest with RandomOverSampling",
-        #     "Random Forest with SMOTENC"
-        # ]
+    with col2:
+        html_content_rf = p1.generate_model_params_html(
+            models[1], random_forest_params, best_params[models[1]]
+            )
+        st.markdown(html_content_rf, unsafe_allow_html=True)
 
+    st.markdown(
+        """<h1 class='h1-sub-title'> Models results on the test set</h1> \n""",
+        unsafe_allow_html=True)
+
+    classifs = st.columns(2, gap="medium")
+    for model_idx, model in enumerate(models):
+        with classifs[model_idx]:
+            for method in methods:
+                full_name = f"{model} (resampling = {str(method)})"
+                st.markdown(f"{full_name}")
+                st.dataframe(classif_reports[model][method])
+    st.table(df_combined)
+
+    st.markdown(
+        """\n <h1 class='h1-sub-title'> If we have to choose ... </h1> \n """,
+        unsafe_allow_html=True)
+
+    choices = st.columns(2)
+    with choices[0]:
         model_choice = st.radio("Choose model", models)
+    with choices[1]:
         method_choice = st.radio("Choose resampling method", methods)
+    model = eval.get_model(model_choice, method_choice)
 
-        display_confusion_matrix(model_name, method_choice, X_test)
-        if model_choice == "Logistic Regression":
-            col1, col2 = st.columns(2)
-            with col1:
-                display_confusion_matrix(model_name, method_choice, X_test)
-            #     st.image('images/cm_lr.png',use_column_width ='always')
-            # with col2:
-            #     st.image('images/roc_lr.png',use_column_width ='always')
-            pass
-        elif model_choice == "Logistic Regression with SMOTENC":
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image('images/cm_lr_smote.png',use_column_width ='always')
-            with col2:
-                st.image('images/roc_lr_smote.png',use_column_width ='always')
-        
-        elif model_choice == "Logistic Regression Classifier with RandomOverSampling":
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image('images/cm_lr_over.png',use_column_width ='always')
-            with col2:
-                st.image('images/roc_lr_over.png',use_column_width ='always')
-        
-        elif model_choice == "Random Forest":
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image('images/roc_rf.png',use_column_width ='always')
-            with col2:
-                st.image('images/imp_rf.png',use_column_width ='always')
-            st.image('images/tree_rf.png',use_column_width ='always')
-        
-        elif model_choice == "Random Forest with RandomOverSampling":
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image('images/roc_rf_over.png',use_column_width ='always')
-            with col2:
-                st.image('images/imp_rf_over.png',use_column_width ='always')
-            st.image('images/tree_rf_over.png',use_column_width ='always')
-        
-        elif model_choice == "Random Forest with SMOTENC":
-            col1, col2 = st.columns(2)
-            with col1:
-                st.image('images/roc_rf_smote.png',use_column_width ='always')
-            with col2:
-                st.image('images/imp_rf_smote.png',use_column_width ='always')
-            st.image('images/tree_rf_smote.png',use_column_width ='always')
+    st.divider()
+    col1, col2 = st.columns(2, gap="medium")
+    with col1:
+        eval.display_confusion_matrix(model, X_test, y_test)
+    with col2:
+        eval.get_roc_auc(model, X_test, y_test)
 
+    if "Random Forest" in model_choice:
+        st.divider()
+        cols = st.columns(2, gap="medium")
+        with cols[0]:
+            top_importances, top_feature_names = eval.get_feature_importance_names(model)
+            eval.plot_feature_importance(top_importances, top_feature_names)
+        with cols[1]:
+            eval.plot_model_tree(model)
 
 with tabs[1]:
     st.write("Deep Learning")
--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import (
-    accuracy_score, classification_report,
+    accuracy_score, classification_report, confusion_matrix,
     roc_curve, roc_auc_score, f1_score
 )
 from sklearn.tree import plot_tree
 import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+import streamlit as st
 
 from src.models.preprocessing import get_feature_names
 
@@ -16,9 +18,13 @@ sns.set_style("whitegrid")
 
 def get_model(model_name, method, remote="https://minio.lab.sspcloud.fr/jbrablx/ai_insurance"):
     method = str(method).lower()
-    with open(f'{remote}/outputs/{model_name}/resampling_{method}.pkl', 'rb') as f:
-        model_used = pickle.load(f)
-    return model_used
+    url = f'{remote}/outputs/{model_name}/resampling_{method}.pkl'
+    response = requests.get(url)
+    if response.status_code == 200:
+        model_used = pickle.loads(response.content)
+        return model_used
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
 
 
 def predict_from_fitted_model(model, X_test, threshold: float = .5):
@@ -42,7 +48,23 @@ def get_metrics(y_test, y_pred, y_proba, threshold: float = .5):
     return metrics_df, classif_report
 
 
-def get_roc_auc(y_test, y_proba):
+def display_confusion_matrix(model, X_test, y_test):
+    y_proba, y_pred = predict_from_fitted_model(model, X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    fig, ax = plt.subplots(figsize=(10, 7))
+    sns.heatmap(
+        cm, annot=True, fmt="d", cmap="Blues",
+        xticklabels=["Predicted 0", "Predicted 1"],
+        yticklabels=["Actual 0", "Actual 1"], ax=ax
+        )
+    ax.set_title("Confusion Matrix")
+    ax.set_ylabel("Actual label")
+    ax.set_xlabel("Predicted label")
+    return st.pyplot(fig)
+
+
+def get_roc_auc(model, X_test, y_test):
+    y_proba, y_pred = predict_from_fitted_model(model, X_test)
     fpr, tpr, thresholds = roc_curve(y_test, y_proba)
     roc_auc = roc_auc_score(y_test, y_proba)
 
@@ -55,10 +77,11 @@ def get_roc_auc(y_test, y_proba):
     ax.set_ylabel('True Positive Rate')
     ax.set_title('Receiver Operating Characteristic (ROC)')
     ax.legend(loc="lower right")
-    return fig
+    return st.pyplot(fig)
 
 
-def get_feature_importance_names(specific_model, model, n: int = 10):
+def get_feature_importance_names(model, n: int = 10):
+    specific_model = model.named_steps['model']
     importances = specific_model.feature_importances_
     indices = np.argsort(importances)[::-1]
 
@@ -78,13 +101,14 @@ def plot_feature_importance(top_importances, top_feature_names):
     ax.set_xticklabels(top_feature_names, rotation=45, ha="right")
     ax.set_xlim([-1, len(top_importances)])
     plt.tight_layout()
-    return fig
+    return st.pyplot(fig)
 
 
-def plot_model_tree(specific_model, model, max_depth=3):
+def plot_model_tree(model, max_depth=3):
+    specific_model = model.named_steps['model']
     estimator = specific_model.estimators_[0]
     feature_names_transformed = get_feature_names(model.named_steps['preprocessor'])
     fig, ax = plt.subplots(figsize=(20, 10))
     plot_tree(estimator, feature_names=feature_names_transformed, class_names=["Class0", "Class1"],
               filled=True, impurity=True, rounded=True, max_depth=max_depth, ax=ax)
-    return fig
+    return st.pyplot(fig)
